0241e68f38b980716972f20d638eb19c96761281
MrKnights1 <eero.vallistu@vikk.ee>
2026-04-04 22:19:12 +0300

    Style: Remap top-right buttons to F23/F24 and hardcode Caps Lock LED to red

--- keyboards/keychron/q3_max/iso_encoder/keymaps/spam_macro/keymap.c
+++ keyboards/keychron/q3_max/iso_encoder/keymaps/spam_macro/keymap.c
@@ -115,7 +115,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______,  _______,  _______,                                _______,                                _______,  _______,  _______,   _______,  _______,   _______, _______),
 
     [WIN_BASE] = LAYOUT_iso_88(
-        KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,    KC_MUTE,  KC_PSCR,  KC_CTANA, RGB_MOD,
+        KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,    KC_MUTE,  KC_PSCR,  KC_F23,   KC_F24,
         KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,    KC_BSPC,  KC_INS,   KC_HOME,  KC_PGUP,
         KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,   KC_ENT,   KC_DEL,   KC_END,   KC_PGDN,
         KC_CAPS,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,  KC_NUHS,
@@ -407,6 +407,9 @@ void matrix_scan_user(void) {
 
 #ifdef RGB_MATRIX_ENABLE
 bool rgb_matrix_indicators_user(void) {
+    if (host_keyboard_led_state().caps_lock) {
+        rgb_matrix_set_color(50, 255, 0, 0);
+    }
     if (spam_enabled) {
         uint8_t r = spam_led_r, g = spam_led_g, b = spam_led_b;
         rgb_matrix_set_color(51, r, g, b);  // A
